07b4eb3e107c35a65f4d8e89e374cce369686ea7
egor_01 <egorvtom12@gmail.com>
2024-12-12 15:51:14 +0700

    rabotal

--- blog/models.py
+++ blog/models.py
@@ -8,3 +8,22 @@ class Article(models.Model):
     text = models.CharField(
         max_length=1000,
         )
+    description = models.TextField(
+        null = True,
+        blank = True,
+        )
+
+    Category = models.ForeignKey(
+        'Category',
+        on_delete=models.SET_NULL,
+         null=True
+        )
+    def __str__(self):
+       return f"Статья: \'{self.title}\'"
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=225,
+    )
+    def __str__(self):
+        return f"Категория: \'{self.name}\'"
